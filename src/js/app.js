516963a8d304a7abdd5f90ebd9ca0636835fc231
Ata Gowani <atagowani@gmail.com>
2017-08-26 16:08:09 -0500

    Improve method of getting quotes

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+var quote = ''
+var author = ''
+
+function loadJSON(callback) {   
+
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType("application/json")
+  xobj.open('GET', './src/data/quotes.json', true)
+  xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          callback(xobj.responseText)
+        }
+  }
+  xobj.send(null)
+}
+
+function newQuote() {
+  loadJSON(function(response) {
+    // Parse JSON string into object
+    var quotes = JSON.parse(response)
+    var randomNumber = Math.random() * Object.keys(quotes).length
+    randomNumber = Math.round(randomNumber)
+    quote = quotes[randomNumber].quote
+    author = quotes[randomNumber].author
+    document.getElementById('quote').innerHTML = quote
+    document.getElementById('author').innerHTML = author
+    console.log(quote)
+  })
+}
+
+newQuote()
