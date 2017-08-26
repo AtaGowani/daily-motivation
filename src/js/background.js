85ddfcb615c9f8d94aa3ac2dafc5064b2d5d0593
Ata Gowani <atagowani@gmail.com>
2017-08-26 16:00:26 -0500

    Redesign

--- src/js/background.js
+++ src/js/background.js
@@ -1,107 +1,38 @@
-function canvasDots(colorLine, colorDot) 
-{
-  var canvas = document.querySelector('canvas'),
-  ctx = canvas.getContext('2d');
+$(document).ready(function(){
+  
+  var mousePos = {}
 
-  console.log(canvas);
-  console.log(ctx);
-  console.log(colorDot);
-  console.log(colorLine);
-
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style.display = 'block';
-  ctx.fillStyle = colorDot;
-  ctx.lineWidth = .1;
-  ctx.strokeStyle = colorLine;
-
-  var mousePosition = {
-    x: 30 * canvas.width / 100,
-    y: 30 * canvas.height / 100
-  };
-
-  var dots = {
-    nb: 600,
-    distance: 50,
-    d_radius: 100,
-    array: []
-  };
-
-  function Dot() {
-  this.x = Math.random() * canvas.width;
-  this.y = Math.random() * canvas.height;
-
-  this.vx = -.5 + Math.random();
-  this.vy = -.5 + Math.random();
-
-  this.radius = Math.random();
+  function getRandomInt(min, max) {
+    return Math.round(Math.random() * (max - min + 1)) + min
   }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX
+    mousePos.y = e.pageY
+  })
+  
+  $(window).mouseleave(function(e) {
+    console.log(window.innerHeight)
+    mousePos.x = -1
+    mousePos.y = -1
+  })
+  
+  setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 50
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");"
+      
+      var sizeInt = getRandomInt(10, 30)
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;"
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;"
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"
 
-  Dot.prototype = {
-  create: function() {
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-  ctx.fill();
-  },
-
-  animate: function() {
-  for (i = 0; i < dots.nb; i++) {
-
-    var dot = dots.array[i];
-
-    if (dot.y < 0 || dot.y > canvas.height) {
-      dot.vx = dot.vx;
-      dot.vy = -dot.vy;
-    } else if (dot.x < 0 || dot.x > canvas.width) {
-      dot.vx = -dot.vx;
-      dot.vy = dot.vy;
+      var style = left+top+color+size
+      $("<div class='ball' style='" + style + "'></div>").appendTo('#wrap')
     }
-    dot.x += dot.vx;
-    dot.y += dot.vy;
-  }
-  },
-
-  line: function() {
-  for (i = 0; i < dots.nb; i++) {
-    for (j = 0; j < dots.nb; j++) {
-      i_dot = dots.array[i];
-      j_dot = dots.array[j];
-
-      if ((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > -dots.distance && (i_dot.y - j_dot.y) > -dots.distance) {
-        if ((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > -dots.d_radius && (i_dot.y - mousePosition.y) > -dots.d_radius) {
-          ctx.beginPath();
-          ctx.moveTo(i_dot.x, i_dot.y);
-          ctx.lineTo(j_dot.x, j_dot.y);
-          ctx.stroke();
-          ctx.closePath();
-        }
-      }
-    }
-  }
-  }
-  };
-
-  function createDots() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (i = 0; i < dots.nb; i++) {
-  dots.array.push(new Dot());
-  dot = dots.array[i];
-
-  dot.create();
-  }
-
-  dot.line();
-  dot.animate();
-  }
-
-  window.onmousemove = function(parameter) {
-  mousePosition.x = parameter.pageX;
-  mousePosition.y = parameter.pageY;
-  }
-
-  mousePosition.x = window.innerWidth / 2;
-  mousePosition.y = window.innerHeight / 2;
-
-  setInterval(createDots, 1000 / 30);
-}
+  }, .1)
+})
