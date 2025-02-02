5ee8a206a0451862ba2301509633eed733ac6f6d
Ata Gowani <atagowani@gmail.com>
2025-02-02 14:18:02 -0600

    Add new dark backgrounds and clean up bubbles

--- src/js/clear.js
+++ src/js/clear.js
@@ -1,20 +1,21 @@
-let clear = (textColor = '#000') => {
+let clear = (textColor = '#000', backgroundColor = '#fff') => {
 
   let body = document.querySelector('body');
   body.style.color = textColor;
+  body.style.background = backgroundColor;
 
   if (document.querySelector('canvas')) { // If there is an existing canvas element
-    let canvas = document.querySelector('canvas'); 
-    
+    let canvas = document.querySelector('canvas');
+
     // Removes the canvas to start over
     document.querySelector('body').removeChild(canvas);
   }
-  
+
   if (document.getElementById('wrap')) { // If the div with dots exists
 
     // Get the div with dots
     let colorfulDiv = document.getElementById('wrap');
-  
+
     // Delete div with dots
     document.querySelector('body').removeChild(colorfulDiv);
   }
